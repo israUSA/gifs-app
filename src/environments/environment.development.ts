63cff0fa395ee9b7a5e8f420c55c4a3537276b6f
ISRAEL <leonardosellan20@gmail.com>
2025-07-01 17:35:29 -0500

    trending avances

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companySlogann: 'Maneja tus gifs'
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App de Gisf',
+  companySlogan: 'Maneja tus gifs'
+
+};
